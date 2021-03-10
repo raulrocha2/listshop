8126eecd9fab50bc826db90b42f9dfbb007cf42c
raulrocha2 <raul.rocha2@gmail.com>
2021-03-10 17:38:00 -0300

    add button para adicionar pela pagina inicial

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import { Card, Button } from 'react-bootstrap'
 import Rating from './Rating'
 import { Link } from 'react-router-dom'
 
@@ -13,8 +13,10 @@ function Product({ product }) {
                 <Link to={`/product/${product._id}`}>
                     <Card.Title as="div">
                         <strong>{product.name}</strong>
+                        
                     </Card.Title>
                 </Link>
+                <Button className='btn-block' disabled={product.countInStock == 0} type='button' size='sm'>Adicionar</Button>
                 <Card.Text as="div">
                     <div className="my-3">
                         <Rating value={product.rating} text={`${product.numReviews} vizualizações`} color={'#f8e825'} />
