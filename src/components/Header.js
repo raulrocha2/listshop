4a707389c46672705571856049aaf21af7891096
raulrocha2 <raul.rocha2@gmail.com>
2021-03-10 16:42:34 -0300

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { 
+    Nav, Navbar, Container, Row, Form, FormControl, Button 
+} from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+
+function Header() {
+    return (
+        <header>
+            <Navbar bg="primary" expand="lg">
+                <Container>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>ListShop</Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <LinkContainer to="/cart">
+                                <Nav.Link>
+                                   
+                                <i className="fas fa-shopping-cart"> </i>
+                                Carrinho
+                                </Nav.Link>
+                            </LinkContainer> 
+                            <LinkContainer to="/login">
+                            <Nav.Link>
+                                <i className="fas fa-user"> </i>
+                                    Login
+                                </Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="btn btn-secondary my-2 my-sm-0">Search</Button>
+                    </Form>
+                    </Navbar.Collapse>
+                </Container>    
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/screens/ProductScreen.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Row, Col, Image, ListGroup, Button, Card, Form } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import Rating from '../components/Rating'
+import products from '../products'
+
+function ProductScreen({ match }) {
+
+    const product = products.find((p) => p._id == match.params.id)
+
+    return (
+        <div>
+           <Link to="/" className='btn btn-light my-5'> Voltar </Link>
+           <Row>
+               <Col md={5}>
+                   <Image src={product.image} alt={product.name} fluid />
+               </Col>
+               <Col md={3}>
+                   <ListGroup variant="flush">
+                       <ListGroup.Item>
+                           <h3>{product.name}</h3>
+                       </ListGroup.Item>
+                       <ListGroup.Item>
+                        <Rating value={product.rating} text={`${product.numReviews} vizualizações`} color={'#f8e825'} />
+                       </ListGroup.Item>
+                       <ListGroup.Item>
+                           Preço: {product.price}
+                       </ListGroup.Item>
+                       <ListGroup.Item>
+                           Descrição: {product.description}
+                       </ListGroup.Item>
+
+                   </ListGroup>
+               </Col>
+               <Col md={3}>
+                   <Card>
+                       <ListGroup variant="flush">
+                           <ListGroup.Item>
+                               <Row>
+                                   <Col>Preço:</Col>
+                                   <Col>
+                                    <stron>{product.price}</stron>
+                                   </Col>
+                               </Row>
+                           </ListGroup.Item>
+                           <ListGroup.Item>
+                                <Row>
+                                    <Col>Status:</Col>
+                                    <Col>
+                                        {product.countInStock > 0 ? 'Estoque' : 'Esgotado'}
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Button className='btn-block' disabled={product.countInStock == 0} type='button'>Adicionar</Button>
+                            </ListGroup.Item>
+
+                       </ListGroup>
+                   </Card>
+               </Col>
+           </Row>
+        </div>
+    )
+}
+
+export default ProductScreen
